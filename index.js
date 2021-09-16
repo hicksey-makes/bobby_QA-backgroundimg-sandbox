cf6e0e8bb7cb70e8942cab5bb184f751dfe8414c
Sam Hicks <hicksey14@gmail.com>
2021-09-16 16:37:32 -0600

    add files

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log(window.innerWidth);
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 480) {
+    document.querySelector("#bobby-head").src="480x800/480x800 Open invitation.png"
+  }
+    else if (window.innerWidth <640) {
+    document.querySelector("#bobby-head").src="720x1280/720x1280 Open invitation.png"
+  } 
+   else if (window.innerWidth <=768) {
+    document.querySelector("#bobby-head").src = "768x1024/768x1024 Open invitation.png";
+  } else if (window.innerWidth <=992) {
+    document.querySelector("#bobby-head").src = "1024x768/1024x768 Open invitation.png"
+  } else if (window.innerWidth <1200) {
+    document.querySelector("#bobby-head").src = "1280x1024/1280x1024 Open invitation.png"
+  } else {
+    document.querySelector("#bobby-head").src = "1920x1080/1920x1080 Open invitation.png"
+  }
+}
+)
